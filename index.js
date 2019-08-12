52e891a451aeac9cf07b935981a8ca41da65b0b1
LLHolmes <lana.lee.holmes@gmail.com>
2019-08-12 14:15:28 -0500

    Add printString function

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// Code your solution here!
-
+const printString = myString => {
+  console.log(myString[0]);
+  if (myString.length > 1) {
+    let subString = myString.substring(1)
+    printString(subString)
+  } else {
+    return true;
+  };
+}
