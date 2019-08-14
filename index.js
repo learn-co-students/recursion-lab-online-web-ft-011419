f0b4f14c7eaf42688ed80b5a88ab2bc5cf5350d7
LLHolmes <lana.lee.holmes@gmail.com>
2019-08-14 13:59:01 -0500

    Add     let subString = string.slice(0, -1); function

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ const printString = myString => {
 
 const reverseString = string => {
   if (string.length > 1) {
-    let subString = string.slice(0, -1);
     return reverseString(string.substring(1)) + string[0];
   } else {
     return string;
   };
+};
+
+const isPalindrome = string => {
+  let l = string.length;
+
+  if (l < 2) {
+    return true;
+  } else if (string[0] === string[l - 1]) {
+    return isPalindrome(string.substring(1, l - 1));
+  } else {
+    return false;
+  }
 };
